6a5fea823cc365e104a21b9ada9084088c1bfbca
João Ernesto Arzamendia <jearzamendia@gmail.com>
2020-01-21 11:53:58 -0400

    fix docker-compose.yml - Mapping

--- dockerfile/build.ps1
+++ dockerfile/build.ps1
@@ -9,4 +9,4 @@ $date = [Xml.XmlConvert]::ToString((get-date),[Xml.XmlDateTimeSerializationMode]
 docker build --build-arg BUILD_DATE=$date --build-arg http_proxy=http://proxy.sgi.ms.gov.br:8081 --build-arg https_proxy=http://proxy.sgi.ms.gov.br:8081 -t $release .
 #docker build --build-arg BUILD_DATE=$date -t $release .
 #push
-#docker push $release
+docker push $release
